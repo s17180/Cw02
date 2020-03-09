80e5897a7d769a193bd5f770a8fc311677c5b455
s17180 <s17180@LAB003-14>
2020-03-09 11:14:33 +0100

    raz

---
+++ Cwiczenie_02/Cwiczenie_02/Model/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Cwiczenie_02.Model
+{
+   public class Student
+
+    {
+        //prop + tabx2
+
+
+            [XmlElement(elementName:"fname")]
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        [XmlAttribute(attributeName:"email")]
+        public string Email { get; set; }
+
+    }
+}
---
+++ Cwiczenie_02/Cwiczenie_02/Program.cs
@@ -0,0 +1,56 @@
+﻿using Cwiczenie_02.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Cwiczenie_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "dane.csv";
+
+            FileInfo f  = new FileInfo(file);
+            // StreamReader stream = new StreamReader(f.OpenRead());
+
+
+            var m = new Dictionary<String, int>();
+
+
+            
+            //virtual - override
+            string file2 = @"Data\dane.csv";
+            using (StreamReader stream = new StreamReader(f.OpenRead())) {
+                String line = "";
+                while ((line = stream.ReadLine()) != null)
+                {
+
+                    string[] studentWiersz = line.Split(',');
+                    Console.WriteLine(line);
+                }
+
+            }
+            // stream.Dispose();
+
+
+            //xml
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), 
+                                       new XmlRootAttribute("uczelnia"));
+
+            var list = new List<Student>();
+            var st = new Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "kowalski@wp.pl"
+            };
+            list.Add(st);
+            serializer.Serialize(writer, list);
+
+        }
+    }
+}
